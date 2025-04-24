a9b1f933acde5002444715a6fea265bf7bb210b8
Filipe Gabriel <filipegabriel11@hotmail.com>
2025-04-24 20:55:43 -0300

    feat - Password change page component created, and template finalized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ChangePasswordComponent } from './pages/change-password/change-password.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent, children: [
-
+    { path: 'change-password', component: ChangePasswordComponent }
   ] },
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
